18e3bc5df5fd143416cdee50aa564404f3cc5add
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2024-10-01 11:26:34 +0200

    Initial commit

---
+++ product-service/src/main/java/fact/it/productservice/service/ProductService.java
@@ -0,0 +1,51 @@
+package fact.it.productservice.service;
+
+import fact.it.productservice.dto.ProductRequest;
+import fact.it.productservice.dto.ProductResponse;
+import fact.it.productservice.model.Product;
+import fact.it.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .skuCode(productRequest.getSkuCode())
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    public List<ProductResponse> getAllProductsBySkuCode(List<String> skuCode) {
+        List<Product> products = productRepository.findBySkuCodeIn(skuCode);
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .skuCode(product.getSkuCode())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
+}
