29e3bfa36601865e9a73927f8b68a408f76ff193
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2024-10-11 12:18:56 +0200

    test

--- product-service/src/main/java/fact/it/productservice/service/ProductService.java
+++ product-service/src/main/java/fact/it/productservice/service/ProductService.java
@@ -4,10 +4,14 @@ package fact.it.productservice.service;
 import fact.it.productservice.dto.ProductResponse;
 import fact.it.productservice.model.Product;
 import fact.it.productservice.repository.ProductRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+
+import static fact.it.productservice.model.Category.FRUIT;
 
 @Service
 @RequiredArgsConstructor
@@ -15,16 +19,32 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-//    public void createProduct(ProductRequest productRequest){
-//        Product product = Product.builder()
-//                .skuCode(productRequest.getSkuCode())
-//                .name(productRequest.getName())
-//                .category(productRequest.getCategory())
-//                .price(productRequest.getPrice())
-//                .build();
-//
-//        productRepository.save(product);
-//    }
+    @PostConstruct
+    public void loadData() {
+        if(productRepository.count() <= 0){
+            Product product = new Product();
+            product.setSkuCode("APPLE001");
+            product.setName("Granny Smith Apple");
+            product.setCategory(FRUIT);
+            product.setPrice(BigDecimal.valueOf(55));
+
+            Product product2 = new Product();
+            product2.setSkuCode("APPLE002");
+            product2.setName("Pink Lady Apple");
+            product2.setCategory(FRUIT);
+            product2.setPrice(BigDecimal.valueOf(100));
+
+            Product product3 = new Product();
+            product3.setSkuCode("PEAR001");
+            product3.setName("Conference Pear");
+            product3.setCategory(FRUIT);
+            product3.setPrice(BigDecimal.valueOf(75));
+
+            productRepository.save(product);
+            productRepository.save(product2);
+            productRepository.save(product3);
+        }
+    }
 
     public List<ProductResponse> getAllProducts() {
         List<Product> products = productRepository.findAll();
