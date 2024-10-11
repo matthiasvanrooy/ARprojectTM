357b96fa344e16cd811298fe7ce877c7bf723c47
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2024-10-11 13:43:18 +0200

    fix

--- product-service/src/main/java/fact/it/productservice/controller/ProductController.java
+++ product-service/src/main/java/fact/it/productservice/controller/ProductController.java
@@ -16,13 +16,6 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct
-            (@RequestBody ProductRequest productRequest) {
-        productService.createProduct(productRequest);
-    }
-
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getProductBySkuCode
