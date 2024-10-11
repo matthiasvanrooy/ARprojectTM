e80c7d427fbaface17d3759ce2e786448e6322c7
Matthias Van Rooy <94641591+matthiasvanrooy@users.noreply.github.com>
2024-10-11 13:01:35 +0200

    changes

--- product-service/src/main/java/fact/it/productservice/service/ProductService.java
+++ product-service/src/main/java/fact/it/productservice/service/ProductService.java
@@ -6,6 +6,8 @@ import fact.it.productservice.model.Product;
 import fact.it.productservice.repository.ProductRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -19,7 +21,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    @PostConstruct
+    @EventListener(ApplicationReadyEvent.class)
     public void loadData() {
         if(productRepository.count() <= 0){
             Product product = new Product();
